acde4e12f1c8d709a55c8e8c30c7bba9735b23e2
Meet Bhanushali <meetbhanushali2001@gmail.com>
2020-11-21 12:22:35 +0530

    initial commit

---
+++ lookup/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Home(request):
+	return render(request,'home.html',{})
+
+def About(request):
+	return render(request,'about.html',{})
